51d8b472ffec34af2cf04684293ab7273b395bde
Prashant-8953 <prashantdubeyvpl0@gmail.com>
2024-02-12 20:57:53 +0530

    some changes

--- weather app/app.js
+++ weather app/app.js
@@ -10,62 +10,49 @@ const wind = document.querySelector(".wind");
 
 
 
-
-	async function checkweather(city){
-		const api_key = "ae9ac0cbca413a6163af12bafbe7cd3b";
-		const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
-
-		const weather_data = await fetch(`${url}`).then(response => response.json());
-		
-
-		if(weather_data.cod === `404`){
-			
-			alert("Please Check Your City Spellings");
-		}
-
-		temp.innerHTML = `${Math.round(weather_data.main.temp-273.15)}&deg;c`;
-
+async function checkweather(city) {
+	const api_key = "ae9ac0cbca413a6163af12bafbe7cd3b";
+	const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
+  
+	try {
+	  const response = await fetch(url);
+	  const weather_data = await response.json();
+  
+	  if (weather_data.cod === "404") {
+		alert("Please Check Your City Spellings");
+	  } else {
+		temp.innerHTML = `${Math.round(weather_data.main.temp - 273.15)}&deg;c`;
 		desc_city.innerHTML = `${weather_data.weather[0].description}`;
-
-
 		humidity.innerHTML = `${weather_data.main.humidity}%`;
-
 		wind.innerHTML = `${weather_data.wind.speed}Km/h`;
-
-		switch(weather_data.weather[0].main){
-			case 'Clouds':
-				weather_img.src = "/images/clouds.png";
-				break;
-				
-			case 'Clear':
-				weather_img.src = "/images/clear.png";
-				break;
-			case 'Rain':
-				weather_img.src = "/images/rain.png";
-				break;
-			case 'Mist':
-				weather_img.src = "/images/mist.png";
-				break;
-			case 'Snow':
-				weather_img.src = "/images/snow.png";
-				break;
-			case 'Drizzle':
-				weather_img.src = "/images/drizzle.png";
-				break;
-
+  
+		switch (weather_data.weather[0].main) {
+		  case "Clouds":
+			weather_img.src = "/images/clouds.png";
+			break;
+		  case "Clear":
+			weather_img.src = "/images/clear.png";
+			break;
+		  case "Rain":
+			weather_img.src = "/images/rain.png";
+			break;
+		  case "Mist":
+			weather_img.src = "/images/mist.png";
+			break;
+		  case "Snow":
+			weather_img.src = "/images/snow.png";
+			break;
+		  case "Drizzle":
+			weather_img.src = "/images/drizzle.png";
+			break;
 		}
-
-		// console.log(weather_data);
-
-
-
-	
+	  }
+	} catch (error) {
+	  console.error("Error fetching weather data:", error);
 	}
-
-searchbtn.addEventListener("click",()=>{
+  }
+  
+  searchbtn.addEventListener("click", () => {
 	checkweather(inputbox.value);
-})
-
-
-
-
+  });
+  
